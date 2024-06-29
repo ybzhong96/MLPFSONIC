f9e933f484661f5e5326f5e3d0aa0aad76f3d49b
Joosep Pata <joosep.pata@cern.ch>
2024-06-29 09:02:33 +0300

    add gen scripts

---
+++ Configuration/Generator/python/SingleK0FlatPt1To1000_pythia8_cfi.py
@@ -0,0 +1,17 @@
+import FWCore.ParameterSet.Config as cms
+generator = cms.EDFilter("Pythia8PtGun",
+                         PGunParameters = cms.PSet(
+        MaxPt = cms.double(1000.),
+        MinPt = cms.double(1.),
+        ParticleID = cms.vint32(130),
+        AddAntiParticle = cms.bool(False), # false in pythia6 version but photons are their own anti-particle
+        MaxEta = cms.double(4.5),
+        MaxPhi = cms.double(3.14159265359),
+        MinEta = cms.double(-4.5),
+        MinPhi = cms.double(-3.14159265359) ## in radians
+        ),
+                         Verbosity = cms.untracked.int32(0), ## set to 1 (or greater)  for printouts
+                         psethack = cms.string('single k0 pt 1 to 1000'),
+                         firstRun = cms.untracked.uint32(1),
+                         PythiaParameters = cms.PSet(parameterSets = cms.vstring())
+                         )
