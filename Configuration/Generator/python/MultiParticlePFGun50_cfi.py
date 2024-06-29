1ebcbe45940b1b0a72bcb410957f60fbd0e42120
Joosep Pata <joosep.pata@cern.ch>
2024-06-29 09:02:33 +0300

    add gen scripts

--- Configuration/Generator/python/MultiParticlePFGun50_cfi.py
+++ Configuration/Generator/python/MultiParticlePFGun50_cfi.py
@@ -2,9 +2,9 @@ import FWCore.ParameterSet.Config as cms
 
 generator = cms.EDProducer("FlatEtaRangeGunProducer",
     PGunParameters = cms.PSet(
-        PartID = cms.vint32([11, -11, 13, -13, 15, -15, 211, -211, 22, 130, -130, 111, -111]),
-        MinEta = cms.double(-4.5),
-        MaxEta = cms.double(4.5),
+        PartID = cms.vint32([11, -11, 13, -13, 211, -211, 22, 130, -130, 111, -111]),
+        MinEta = cms.double(-5),
+        MaxEta = cms.double(5),
         MinPhi = cms.double(-3.14159265359),
         MaxPhi = cms.double(3.14159265359),
         MinE = cms.double(1.0),
