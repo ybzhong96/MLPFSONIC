166cb86f79b3d91ed65e1eb7e1d5539c34e51479
Joosep Pata <joosep.pata@cern.ch>
2024-07-15 14:20:20 +0300

    disable cuda for now

--- RecoParticleFlow/PFProducer/plugins/MLPFProducer.cc
+++ RecoParticleFlow/PFProducer/plugins/MLPFProducer.cc
@@ -192,7 +192,7 @@ void MLPFProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions
   desc.add<edm::FileInPath>("model_path",
                             edm::FileInPath("RecoParticleFlow/PFProducer/data/mlpf/"
                                             "mlpf_21M_attn2x6x512_bs40_relu_tt_qcd_zh400k_checkpoint25_1xa100_fp32_fused.onnx"));
-  desc.add<bool>("use_cuda", true);
+  desc.add<bool>("use_cuda", false);
   descriptions.addWithDefaultLabel(desc);
 }
 
